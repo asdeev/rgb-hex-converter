887cd9061c8a826cbcfd406521a44091c834f8c6
Zaid Bhura <3915682+asdeev@users.noreply.github.com>
2018-12-26 10:55:40 -0800

    Updated logic

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 function processRGB(e) {
-    console.log(e);
-    console.log(this.value);
+    const initial = Math.floor(this.value / 16);
+    const rem = this.value % 16;
+    const rgb = `${initial.toString(16)}${rem.toString(16)}`;
+    console.log(rgb);
 }
 
 function processHex(e) {
-    console.log(e);
-    console.log(this.value);
+    const [ initial, rem ] = this.value.split('');
+    const hexInitial = parseInt(initial, 16) * 16;
+    const hexRem = rem ? parseInt(rem, 16) : 0;
+    const hex = hexInitial + hexRem;
+    console.log(hex);
 }
 
 const rgbInput = document.querySelector('#rgb');
 const hexInput = document.querySelector('#hex');
 
-rgbInput.addEventListener('keydown', processRGB);
-hexInput.addEventListener('keydown', processHex);
+rgbInput.addEventListener('keyup', processRGB);
+hexInput.addEventListener('keyup', processHex);
